713c161151242635162579441a3781c5a0f66e48
Erik Erlandson <eerlands@redhat.com>
2020-04-05 16:58:34 -0700

    NonNegative testing

--- coulomb-tests/src/test/scala/coulomb/refined.scala
+++ coulomb-tests/src/test/scala/coulomb/refined.scala
@@ -27,13 +27,26 @@ import eu.timepit.refined.numeric._
 
 import coulomb._
 import coulomb.si._
+import coulomb.us._
 
 import coulomb.validators.CoulombValidators._
 
 object RefinedTests extends TestSuite {
   val tests = Tests {
     test("refined NonNegative") {
-      assert(refineMV[NonNegative](0.0).withUnit[Meter].isValidQ[Refined[Double, NonNegative], Meter](0))
+      val q1 = refineMV[NonNegative](0.0).withUnit[Meter]
+      val q2 = refineMV[NonNegative](0.0).withUnit[Foot]
+      assert(q1.isValidQ[Refined[Double, NonNegative], Meter](0))
+      assert(q2.isValidQ[Refined[Double, NonNegative], Foot](0))
+      val q3 = refineMV[NonNegative](1.0).withUnit[Meter]
+      val q4 = refineMV[NonNegative](1.0).withUnit[Foot]
+      val q5 = refineMV[NonNegative](2.0).withUnit[Foot]
+      assert((q3 + q4).isValidQ[Refined[Double, NonNegative], Meter](1.3048))
+      assert((q4 + q3).isValidQ[Refined[Double, NonNegative], Foot](4.2808))
+      assert((q1 * q3).isValidQ[Refined[Double, NonNegative], Meter %^ 2](0))
+      assert((q1 / q3).isValidQ[Refined[Double, NonNegative], Unitless](0))
+      assert((q3 * q5).isValidQ[Refined[Double, NonNegative], Meter %* Foot](2))
+      assert((q3 / q5).isValidQ[Refined[Double, NonNegative], Meter %/ Foot](0.5))
     }
   }
 }
